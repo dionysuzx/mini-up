5d51556945f33d404958a1061797318c6e81e516
dionysuz <13951458+dionysuzx@users.noreply.github.com>
2025-04-21 02:29:38 -0400

    add shadcn-svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 import { sdk } from "@farcaster/frame-sdk";
 import { onMount } from "svelte";
+import { Button } from "$lib/components/ui/button/index.js";
 
 onMount(async () => {
     await sdk.actions.ready();
@@ -9,3 +10,4 @@ onMount(async () => {
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Button>Click me</Button>
