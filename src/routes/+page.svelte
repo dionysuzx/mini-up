22aef14d20421b606741b5756dc8b08cd8401615
dionysuz <13951458+dionysuzx@users.noreply.github.com>
2025-04-21 01:58:45 -0400

    fixup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script>
+import { sdk } from "@farcaster/frame-sdk";
+import { onMount } from "svelte";
+
+onMount(async () => {
+    await sdk.actions.ready();
+});
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
